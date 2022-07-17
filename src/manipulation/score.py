25cb2824a658db40b611edca58c9fc67b7bc5ab5
gentoopenguin1115 <saitokei322@gmail.com>
2022-07-17 13:20:44 +0900

    [add] scoreファイル Totalスコアのところにその日の滞在時間を追加していく

---
+++ src/manipulation/score.py
@@ -0,0 +1,44 @@
+import os
+from time import time
+from tracemalloc import start
+from dotenv import load_dotenv
+import os.path
+load_dotenv()
+import datetime
+from datetime import datetime
+from oauth2client.service_account import ServiceAccountCredentials
+import gspread
+import pandas as pd
+
+
+def get_sheet(gss_sheet):
+    json_path = os.getenv('JSON_KEY')
+    gss_key=os.getenv('SPREADSHEET_KEY')
+    gss_sheet=gss_sheet
+    SCOPES = ['https://spreadsheets.google.com/feeds','https://www.googleapis.com/auth/drive']
+    cred = ServiceAccountCredentials.from_json_keyfile_name(json_path, SCOPES)
+    gs_auth = gspread.authorize(cred)
+    sh=gs_auth.open_by_key(gss_key)
+    ws=sh.worksheet(gss_sheet)
+    return ws
+
+date=datetime.today()
+def get_data(date,name):
+    name_dict=name_dict = {"O":2, "K":3,"S":4,"N":5}
+    name_p=name_dict[name]
+    month=date.month
+    date_p=date.day
+    ws=get_sheet( f'{month}月')
+    score = ws.cell(34,name_p).value
+    today_time= ws.cell(date_p+2,name_p).value
+    E=datetime.strptime(today_time[6:],'%H:%M')
+    L=datetime.strptime(today_time[:5],'%H:%M')
+    today_score=(E-L).total_seconds()/3600
+    re_score=today_score+float(score)
+    re_score='{:.2f}'.format(re_score)
+    ws.update_cell(34,name_p, re_score)
+    return re_score
+
+
+print(get_data(date,"O"))
+
