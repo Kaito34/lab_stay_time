72cd55e5d787b8acc184fa23d2e7e1111a92233b
gentoopenguin1115 <saitokei322@gmail.com>
2022-07-17 14:09:52 +0900

    [updater] データのグラフ化を付け加えました

--- src/manipulation/score.py
+++ src/manipulation/score.py
@@ -1,6 +1,5 @@
 import os
-from time import time
-from tracemalloc import start
+import numpy as np
 from dotenv import load_dotenv
 import os.path
 load_dotenv()
@@ -8,9 +7,7 @@ import datetime
 from datetime import datetime
 from oauth2client.service_account import ServiceAccountCredentials
 import gspread
-import pandas as pd
-
-
+import matplotlib.pyplot as plt
 def get_sheet(gss_sheet):
     json_path = os.getenv('JSON_KEY')
     gss_key=os.getenv('SPREADSHEET_KEY')
@@ -23,11 +20,32 @@ def get_sheet(gss_sheet):
     return ws
 
 date=datetime.today()
+
+def gragh(data,name,month):
+    plt.figure(figsize=(6,4))
+    plt.plot(np.arange(len(data)),data)
+    plt.xlabel('date')
+    plt.ylabel(' score')
+    plt.xlim(0, 32)
+    plt.ylim(0, 140)
+    
+    plt.savefig(f'../../data/score_data/{name}_{month}.png')
+    plt.show()
+
 def get_data(date,name):
-    name_dict=name_dict = {"O":2, "K":3,"S":4,"N":5}
+    name_dict=name_dict = {"orui":2, "kusumoto":3,"saito":4,"nomura":5}
+    if date.day== 1:
+        record=0
+    else:
+        record=np.load(f'../../data/score_data/{name}_score.npy')
     name_p=name_dict[name]
     month=date.month
     date_p=date.day
+    if len(record)<date_p:
+        record=np.append(record,np.array([record[-1]]*(date_p-len(record)-1)))
+        print(len(record))
+    if len(record)>date_p:
+        print("error")
     ws=get_sheet( f'{month}月')
     score = ws.cell(34,name_p).value
     today_time= ws.cell(date_p+2,name_p).value
@@ -35,10 +53,13 @@ def get_data(date,name):
     L=datetime.strptime(today_time[:5],'%H:%M')
     today_score=(E-L).total_seconds()/3600
     re_score=today_score+float(score)
+    record=np.append(record,re_score)
+    gragh(record,name,date.month)
+    np.save(f'../../data/score_data/{name}_score',record)
     re_score='{:.2f}'.format(re_score)
     ws.update_cell(34,name_p, re_score)
-    return re_score
+    return 
 
 
-print(get_data(date,"O"))
+get_data(date,"orui")
 
